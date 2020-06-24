82d64bed9e567374952b687c1e8fff6fc9946d94
raj u <r@Rs-MacBook-Air.local>
2020-06-24 10:01:22 -0700

    appjs

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Title from "./components/Title";
+
+
+function App() {
+  return (
+    <div className="app_container">
+      <Title />
+     
+    </div>
+  );
+}
+
+export default App;
---
+++ components/Titlle.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Title() { 
+
+  return(
+    <div className="title_container">
+      <h1>Conway's Game of Life</h1>
+    </div>
+  )
+  }
